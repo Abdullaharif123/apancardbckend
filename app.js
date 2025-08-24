7644a81589002be7836dbabc20ffb32d36a7a117
Abdullaharif123 <152612204+Abdullaharif123@users.noreply.github.com>
2025-08-24 16:14:28 +0500

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 // ----------------------
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://apna-card.vercel.app"], // ✅ no trailing slash
+    origin: ["http://localhost:3000", "https://apnacard.vercel.app"], // ✅ no trailing slash
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
