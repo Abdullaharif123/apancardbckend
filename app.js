e7d5a57bd03381dd942c4ac862d322e49b6ab502
Abdullaharif123 <152612204+Abdullaharif123@users.noreply.github.com>
2025-08-24 15:38:33 +0500

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 // ✅ Allow all origins
 app.use(
   cors({
-    origin: "*", // <-- allow all
+    origin: ["http://localhost:3000", "https://apna-card.vercel.app"], // <-- allow all
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
